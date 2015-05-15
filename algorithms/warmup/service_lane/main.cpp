c640aa112d68edc3510aa5cd820ec118c7219a1b
Grant Freeman <grantfree035@gmail.com>
2015-05-15 10:23:37 -0500

    base code added for service lane

---
+++ algorithms/warmup/service_lane/main.cpp
@@ -0,0 +1,37 @@
+// Author: Grant Freeman
+// Domain: hackerrank.com
+// Problem: Service Lane
+
+#include <iostream>
+using namespace std;
+
+int main ()
+  {
+  // declare variables
+  int i, // entry index
+      j, // exit index
+      n, // length of lane
+      tc, // number of test cases
+      w; // width
+  
+  cin >> n >> tc;
+  int * sl = new int [n];
+  
+  for (int k(0); i < N; ++k)
+    cin >> sl [k];
+  
+  for (int k(0); k < tc; ++k)
+    {
+    cin >> sl [i];
+    w = sl [i-1];
+    
+    for (int m=i; m < j; ++m)
+      if (w > sl[m])
+        w = sl [m];
+    
+    cout << w << " ";
+    }
+  
+  cout << endl;
+  return 0;
+  }
