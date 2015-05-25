dab3cd888dd568a6376e5c88f272e3825537b50e
Grant Freeman <grantfree035@gmail.com>
2015-05-25 11:55:47 -0500

    reduced code with max_subarray methods: not debugged

--- algorithms/warmup/the_maximum_subarray/C/main.cpp
+++ algorithms/warmup/the_maximum_subarray/C/main.cpp
@@ -1,3 +1,5 @@
+
+#include "max_subarray.h"
 #include <iostream>
 using namespace std;
 
@@ -25,119 +27,17 @@ int main ()
     for (int j=0; j < len; ++j)
       cin >> arr [j];
     
-    /******************************************/
-    /***    find Max Contiguous Subarray    ***/
-    /******************************************/
+    // find largest contiguous subarray
+    max = max_subarr_contig (arr, len);
     
-    // set initial values
-    cur_sum = 0;
-    cur_ind = -1;
-    best_sum = 0;
-    best_start_ind = -1;
-    best_end_ind = -1;
+    // print max subarray
+    cout << max << " ";
     
+    // find max non-contiguous sub array
+    max = max_subarr_non_contig (arr, len);
     
-    // find max contiguous subarray
-    for (int j=0; j < len; ++j)
-      {
-      // get next sum
-      n = cur_sum + arr [j];
-      
-      // check that sum is positive
-      if (n > 0)
-        {
-        // set current index if cur_sum is 0
-        if (cur_sum == 0)
-          cur_ind = j;
-        
-        // set current sum
-        cur_sum = n;
-        }
-      else
-        cur_sum = 0;
-      
-      // check if current sum is best sum
-      if (cur_sum > best_sum)
-        {
-        // set new best sum
-        best_sum = cur_sum;
-        
-        // set indexes
-        best_start_ind = cur_ind;
-        best_end_ind = j;
-        }
-      }
-    
-    // Check if an contiguous array was found
-    // if not: then find the largest num in the array
-    
-    // this is smallest number that may be given
-    best_num = -10000;
-    
-    if (best_sum == 0)
-      {
-      // find largest num
-      for (int j=0; j < len; ++j)
-        {
-        // save num for min overhead
-        n = arr [j];
-        
-        // check if num is best num
-        if (n > best_num)
-          best_num = n;
-        else;
-        }
-      
-      // print largest num
-      cout << best_num << " ";
-      }
-    else
-      {
-      // largest contiguous sum exist: print sum
-      cout << best_sum << " ";
-      }
-    
-    /******************************************/
-    /***  find Max Non-Contiguous Subarray  ***/
-    /******************************************/
-    
-    // reset max
-    max = 0;
-    
-    for (int j=0; j < len; ++j)
-      {
-      // save value in temp var, less overhead call
-      n = arr [j];
-      
-      // if var is possitive, add to max
-      if (n > 0)
-        max += n;
-      }
-    
-    if (max == 0)
-      {
-      // find the largest integer in array
-      max = -1000;
-      
-      // iterate through array
-      for (int j=0; j < len; ++j)
-        {
-        // principle of locality
-        n = arr [j];
-        
-        // check if in is largest max
-        if (n > max)
-          max = n;
-        }
-      
-      // print largest num found
-      cout << max << endl;
-      }
-    else
-      {
-      // largest sub-arrary exist: print max value
-      cout << max << endl;
-      }
+    // print max
+    cout << max << endl;
     
     // clean up memory
     delete [] arr;
